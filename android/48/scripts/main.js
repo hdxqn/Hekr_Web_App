f2dae777b8c31b93a28e3ee1d64a2952f8a74ac3
hdxqn <314123010@qq.com>
2015-09-01 16:14:23 +0800

    光照颜色传感器（48） 描述1.完成传感器页面以及功能

---
+++ android/48/scripts/main.js
@@ -0,0 +1,197 @@
+$(document).ready(function(){
+	touchEvents={};
+	browserRedirect(touchEvents);
+	var lang=getUrlParam('lang');
+	switch(lang){
+		case 'en-US':
+		lang='en-US';
+		break;
+		case 'zh-CN':
+		lang='zh-CN';
+		break;
+		default:
+		lang='en-US';
+		break;
+	}
+
+		i18n.init({
+        "lng": lang,
+        "resStore": resources,
+        "fallbackLng" : 'en'
+   	 }, function (t) {
+        $(document).i18n();
+    	});
+    	
+  
+  $("#modal").modal({escapeClose: !1,clickClose: !1,showClose: !1});
+   t = new Toast({
+     			message:i18n.t("message")
+ 			});
+});
+
+function browserRedirect(obj) {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+         
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+         obj.touchstart="touchstart";
+            obj.touchmove= "touchmove";
+            obj.touchend= "touchend";
+         
+    } else {
+        
+                obj.touchstart = "mousedown";
+                obj.touchmove = "mousemove";
+                obj.touchend = "mouseup";
+         
+    }
+ }
+
+
+
+var resources={
+	"zh-CN":{
+		"translation":{
+			"hekr":"HEKR",
+			"about":"关于",
+			"light":"光:",
+			"connecting":"拼命连接中...",
+			"off":"关",
+			"on":"开",
+			"message":"指令已发送"
+		}
+	},
+	"en-US":{
+		"translation":{
+			"hekr":"HEKR",
+			"about":"about",
+			"light":"light:",
+			"connecting":"connecting...",
+			"off":"off",
+			"on":"on",
+			"message":"sended"
+		}
+	}
+};
+
+
+
+function numTransformate(value){
+	if(typeof(value)=="number"){
+		value = UARTDATA.hex2str(value);
+	}else{
+		value = parseInt(value,10);
+		value = UARTDATA.hex2str(value);
+	}
+	return value;
+}
+
+
+ function getUrlParam(name) {
+     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if (r != null) return unescape(r[2]);
+     return null;
+ }
+var Toast = function(config){
+	this.context = config.context || $('body');
+	this.message =config.message;
+	this.time = config.time || 3000;
+	this.init();
+}
+
+var msgEntity;
+Toast.prototype = {
+	init :function(){
+		$("#toastMessage").remove();
+
+		var msgDIV = new Array();
+		msgDIV.push('<div id="toastMessage">');
+		msgDIV.push('<span>'+this.message+'</span>');
+		msgDIV.push('</div>');
+		msgEntity = $(msgDIV.join('')).appendTo(this.context);
+		msgEntity.hide();
+	},
+
+	show :function(){
+		msgEntity.stop(true);
+		var left = msgEntity.find('span').width()/2 ;
+		msgEntity.css("margin","0 0 0 -"+left+"px");
+		msgEntity.fadeIn(this.time/2);
+		msgEntity.fadeOut(this.time/2);
+		
+	}
+}
+function setLightState(a,b,c){
+	$("#light").css("background-color","rgb("+a+","+a+","+a+")");
+	$("#lightNum").text(b+""+c);
+}
+function setRgbState(r,g,b){
+	$("#rgb").css("background-color","rgb("+r+","+g+","+b+")");
+	$("#rNum").text(r);
+	$("#gNum").text(g);
+	$("#bNum").text(b);
+}
+
+  
+
+
+
+var tid  = getUrlParam("tid");
+var host = getUrlParam("host") || "device.hekr.me";
+
+var token =getUrlParam("access_key") ;
+
+var user = Math.floor(Math.random()*100);
+var url  = "ws://"+host+":8080/websocket/t/"+user+"/code/"+token+"/user";
+var ws   = new ReconnectingWebSocket(url);
+
+
+ws.onmessage=function(e){
+	console.debug("[WEBSOCKET] "+e.data);
+	SEXP.exec(e.data);
+};
+
+ws.onerror=function(){
+	
+	console.error("[WEBSOCKET] connection error");
+};
+
+ws.onopen=function(){
+	console.debug("[WEBSOCKET] connection opened");
+	 var data="0000000000000000",
+	    frame=UARTDATA.encode(0x02,data); 
+	var code ='(@devcall "{tid}" (uartdata "{args}") (lambda (x) x))'
+			.replace('{tid}',tid)
+			.replace('{args}',frame);
+		setTimeout(function(){
+			 ws.send(code);
+			},500);	
+   console.debug(code);
+	$.modal.close();
+};
+ws.onclose=function(){
+	console.error("[WEBSOCKET] connection closed");
+};
+
+window.changestate=function(e){
+	
+console.debug("[STATE] ================");
+     console.debug(e); 
+     console.debug("[STATE] ================");
+     console.debug(e.uartdata);
+     if(e.tid===tid){
+     	var mes=UARTDATA.decode(e.uartdata);
+     		console.debug(mes);
+     		setLightState(mes[4],mes[5],mes[6]);
+     		setRgbState(mes[1],mes[2],mes[3]);
+     	}	
+};
+
